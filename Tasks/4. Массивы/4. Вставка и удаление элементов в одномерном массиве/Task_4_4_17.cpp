91171e4c957056801f95a0e42cc58c71ed0e5715
artjomereomin2 <artjomereomin2@yandex.ru>
2024-11-06 10:11:02 +0400

    Удаление в одномерном массиве

---
+++ Tasks/4. Массивы/4. Вставка и удаление элементов в одномерном массиве/Task_4_4_17.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int d;
+	do {
+		setlocale(LC_ALL, "Rus");
+		// Инициализация маcсива
+		int n;
+		cout << "Введите n: ";
+		cin >> n;
+		int* m = new int[2 * n];
+		// Ввод эл-ов массива, Х и Y
+		cout << "Введите элементы массива:" << endl;
+		for (int i = 0; i < (n); i++) {
+			cin >> m[i];
+		}
+		int x;
+		cout << "Введите Х: ";
+		cin >> x;
+		int y;
+		cout << "Введите Y: ";
+		cin >> y;
+		// Изменение массива (при нахождении необходимого эл-та сдвигаем
+		// все эл-ты после него на 1 вперёд и запоминаем в счётчике этот сдвиг
+		int c = 0;
+		for (int i = 0; i < n + c; i++) {
+			if (m[i] == x) {
+				for (int j = n + c - 1; j >= i; j--) {
+					m[j + 1] = m[j];
+				}
+				m[i] = y;
+				c++;
+				i++;
+			}
+		}
+		// Вывод изменённого массива
+		cout << "Массив c Y перед X:\n";
+		for (int i = 0; i < n + c - 1; i++) {
+			cout << m[i] << ", ";
+		}
+		cout << m[n + c - 1] << endl;
+		// Удаление массива
+		delete[]m;
+
+		cout << "Введите любое число для продолжения (кроме нуля): ";
+		cin >> d;
+	} while (d);
+	return 0;
+}
